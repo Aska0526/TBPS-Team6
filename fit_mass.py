09b2c3b3aee09a2786f0f7ea348f30a1c87d973f
chun9l <97897047+chun9l@users.noreply.github.com>
2022-01-31 22:58:24 +0000

    Added cell to find ratio of backgrd and signal. NOT SURE IF ITS 100% WORKING OR CORRECT.

--- fit_mass.py
+++ fit_mass.py
@@ -42,6 +42,7 @@ def combined(x, a, b, mu_exp, sigma, A, mu_gauss):
     return bckgrd(x, a, b, mu_exp) + gaussian(x, sigma, A, mu_gauss)
 
 
+td = pd.read_pickle(r'year3-problem-solving\total_dataset.pkl')
 df_5 = pd.read_pickle(r'chi^2 filtered data\td 5% (1).pkl')
 df_10 = pd.read_pickle(r'chi^2 filtered data\td 10%.pkl')
 df_30 = pd.read_pickle(r'chi^2 filtered data\td 30%.pkl')
@@ -84,4 +85,48 @@ f_q2, axs_q2 = plt.subplots(3, 1)
 axs_q2[0].hist(q_sq[0], bins=200)
 axs_q2[1].hist(q_sq[1], bins=200)
 axs_q2[2].hist(q_sq[2], bins=200)
+plt.show()
+
+#%%
+"""
+Should (?) show plot the signal noise ratio versus the chi_sq threshold
+(whatever that means...)
+"""
+
+
+def full_filter(file, thres, column=None):
+    """
+    Selects only the likely candidates (maybe?)
+    The column arg can be left empty, otherwise can choose the column u want
+    """
+    file_fild = file[file['Kstar_MM'] > 800]
+    file_fild = file_fild[file_fild['B0_IPCHI2_OWNPV'] > thres]
+    file_fild = file_fild[file_fild['Pi_IPCHI2_OWNPV'] > thres]
+    file_fild = file_fild[file_fild['B0_FDCHI2_OWNPV'] > thres]
+    if column is None:
+        return file_fild
+    else:
+        return file_fild[column]
+
+
+def log_likelihood(thres):
+    """
+    NOT THE LOG LIKELIHOOD FUNCTION YET...
+    This will fit the chopped data and finds the ratio of signal and noise
+    """
+    p0 = [2000, 0.0003, 5200, 50, 2000, 5300]
+    result = []
+    for i in thres:
+        fil_data = full_filter(td, i, 'B0_MM')
+        height, edges, _ = plt.hist(fil_data, bins=200, range=[5180, 5700])
+        popt, pcov = curve_fit(combined, edges[:-1], height, p0=p0)
+        r = popt[4] / popt[0]
+        result.append(r)
+    plt.close('all')
+    return result
+
+
+threshold = np.linspace(0.1, 6, 100)
+
+plt.plot(threshold, log_likelihood(threshold), 'x')
 plt.show()
