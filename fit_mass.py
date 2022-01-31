b5ef3f78918967ae916aab667cb19292028b7897
chun9l <97897047+chun9l@users.noreply.github.com>
2022-01-31 20:59:25 +0000

    Added cell to plot hist of q^2

--- fit_mass.py
+++ fit_mass.py
@@ -52,29 +52,36 @@ B_mass = np.array([df_5['B0_MM'], df_10['B0_MM'], df_30['B0_MM']], dtype=object)
 Setup the axes for histogram 
 and perform curve fit
 """
-f, axs = plt.subplots(3, 1)  # Creates a 3 row by 1 column plot
-heights_5, edges, _ = axs[0].hist(B_mass[0], bins=200, range=[5180, 5700], label='data')
-heights_10, edges, _ = axs[1].hist(B_mass[1], bins=200, range=[5180, 5700], label='data')
-heights_30, edges, _ = axs[2].hist(B_mass[2], bins=200, range=[5180, 5700], label='data')
+f_B0, axs_B0 = plt.subplots(3, 1)  # Creates a 3 row by 1 column plot
+heights_5, edges, _ = axs_B0[0].hist(B_mass[0], bins=200, range=[5180, 5700], label='data')
+heights_10, edges, _ = axs_B0[1].hist(B_mass[1], bins=200, range=[5180, 5700], label='data')
+heights_30, edges, _ = axs_B0[2].hist(B_mass[2], bins=200, range=[5180, 5700], label='data')
 # plt.rcParams['text.usetex'] = True
-axs[0].set_title('5% sig level')
-axs[1].set_title('10% sig level')
-axs[2].set_title('30% sig level')
-f.supxlabel(r'B0 mass $\left(\frac{MeV}{c^2}\right)$')  # The overall x label
-f.supylabel('Number')
-f.tight_layout()  # Prevents the plots from overlapping
+axs_B0[0].set_title('5% sig level')
+axs_B0[1].set_title('10% sig level')
+axs_B0[2].set_title('30% sig level')
+f_B0.supxlabel(r'B0 mass $\left(\frac{MeV}{c^2}\right)$')  # The overall x label
+f_B0.supylabel('Number')
+f_B0.tight_layout()  # Prevents the plots from overlapping
 
 p0 = [2100, 0.0003, 5180, 50, 2000, 5300]
 x = np.array([(edges[i] + edges[i + 1]) / 2 for i in range(len(edges) - 1)])
 popt_5, pcov_5 = curve_fit(combined, x, heights_5, p0=p0)
 popt_10, pcov_10 = curve_fit(combined, x, heights_10, p0=p0)
 popt_30, pcov_30 = curve_fit(combined, x, heights_30, p0=p0)
-axs[0].plot(x, combined(x, *popt_5), label='fit')
-axs[0].legend()
-axs[1].plot(x, combined(x, *popt_10), label='fit')
-axs[2].plot(x, combined(x, *popt_30), label='fit')
+axs_B0[0].plot(x, combined(x, *popt_5), label='fit')
+axs_B0[0].legend()
+axs_B0[1].plot(x, combined(x, *popt_10), label='fit')
+axs_B0[2].plot(x, combined(x, *popt_30), label='fit')
 plt.show()
 
-
 #%%
-
+"""
+Plots histogram of the q^2 distribution (crudely)
+"""
+q_sq = np.array([df_5['q2'], df_10['q2'], df_30['q2']], dtype=object)
+f_q2, axs_q2 = plt.subplots(3, 1)
+axs_q2[0].hist(q_sq[0], bins=200)
+axs_q2[1].hist(q_sq[1], bins=200)
+axs_q2[2].hist(q_sq[2], bins=200)
+plt.show()
