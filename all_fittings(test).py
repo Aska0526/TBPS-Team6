83a43c6b9291c91a6ba0842a215d83c303bda499
Chung Ming Loi <97897047+chun9l@users.noreply.github.com>
2022-03-15 22:23:35 +0000

    Add files via upload

--- all_fittings(test).py
+++ all_fittings(test).py
@@ -7,6 +7,7 @@ from scipy.integrate import trapezoid
 from pathlib import Path
 from mpl_toolkits import mplot3d
 
+
 def bckgrd(x, a, b, mu_exp):
     """
     The exponentially decaying background
@@ -61,24 +62,24 @@ def bin_num(dataset, num):
         return dataset
 
 
-def poly(x, a0, a1, a2, a3, a4, a5, a6):  # doesnt get much better after order 6
+def poly(x, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12):  # doesnt get much better after order 6
     """
     set the jth coeff of the power series by aj
     has to do it this way since curve_fit unpacks p0 automatically
     np.polynomial.Polynomial sets up the power series in ascending order
     """
-    return np.polynomial.Polynomial([a0, a1, a2, a3, a4, a5, a6])(x)
+    return np.polynomial.Polynomial([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12])(x)
 
 
 def d2gamma_p_d2q2_dcostheta(fl, afb, cos_theta_l, bn):
     ctl = np.array(cos_theta_l)
     c2tl = 2 * (ctl ** 2) - 1
     array = (3 / 8) * ((3 / 2) - (fl * 1 / 2) + (c2tl * 1 / 2) * (1 - 3 * fl) + (8 / 3) * afb * ctl)
-    if np.min(array) <= 0.0:
+    # if np.min(array) <= 0.0:
         # del_index = np.where(array <= 0.0)  # Method 1 (delete invalid ones)
         # array = np.delete(array, del_index)
         # ctl = np.delete(ctl, del_index)
-        array = array - (np.min(array))  # Method 2 (shift up)
+        # array = array - (np.min(array))  # Method 2 (shift up)
         # array = abs(array)  # Method 3 (take absolute)
     A0 = trapezoid(array, ctl)
     array *= poly(ctl, *cof_mt[bn])
@@ -100,7 +101,7 @@ fl_val_theo = [0.296, 0.76, 0.796, 0.711, 0.607, 0.348, 0.328, 0.435, 0.748, 0.3
 fl_err_theo = [0.05, 0.04, 0.03, 0.05, 0.05, 0.04, 0.03, 0.04, 0.04, 0.02]
 #%%
 # Reads the total dataset and apply some manual cuts
-ds = pd.read_pickle(Path(r'year3-problem-solving/XGB_filter_signal_td_depth_9_equal_oversampling.pkl'))
+ds = pd.read_pickle(Path(r'year3-problem-solving/XGB_filter_signal_td_depth_15_equal_oversampling.pkl'))
 # choose candidates with one muon PT > 1.7GeV
 PT_mu_filter = (ds['mu_minus_PT'] >= 1.7 * (10 ** 3)) | (ds['mu_plus_PT'] >= 1.7 * (10 ** 3))
 
@@ -150,7 +151,7 @@ ds_filtered = ds[
     #& pi_to_be_K_filter
     #& K_to_be_pi_filter
     #& pi_to_be_p_filter
-]
+    ]
 
 #%%
 # Fits and plots B0_MM distribution
@@ -169,11 +170,12 @@ plt.show()
 
 #%%
 # Fits the acceptance dataset with 6th order poly
-amc = pd.read_pickle(Path(r'year3-problem-solving/acceptance_mc.pkl'))
+amc = pd.read_pickle(Path(r'year3-problem-solving/XGB_filter_signal_acceptancemc_depth_15_equal_oversampling.pkl'))
 
 cof_mt = []
 
 # Do this for all 10 predefined bins
+order = 12
 for bn in range(10):
     plt.figure()
     ctl = bin_num(amc, bn)['costhetal']
@@ -181,22 +183,34 @@ for bn in range(10):
     plt.close()  # Comment out this if u want to see the histograms
     x = np.array([(edges[i] + edges[i + 1]) / 2 for i in range(len(edges) - 1)])  # Finds centre of each HISTOGRAM bin
 
-    x_array = np.array([[x[0] ** i for i in range(7)],  # Defines the matrix for costhetal.
-                        [(x[len(edges) // 10]) ** i for i in range(7)],
-                        [(x[len(edges) // 2]) ** i for i in range(7)],
-                        [(x[len(edges) // -5]) ** i for i in range(7)],
-                        [(x[len(edges) // -9]) ** i for i in range(7)],
-                        [(x[int(len(edges) // 1.1)]) ** i for i in range(7)],
-                        [x[-1] ** i for i in range(7)]])
+    x_array = np.array([[x[0] ** i for i in range(order + 1)],  # Defines the matrix for costhetal.
+                        [(x[int(len(edges) // 10)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // 2)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // -5)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // -7)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // -1.1)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // 4)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // 6)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // 9)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // -3)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // -9)]) ** i for i in range(order + 1)],
+                        [(x[int(len(edges) // -3.5)]) ** i for i in range(order + 1)],
+                        [x[-1] ** i for i in range(order + 1)]])
 
     y_array = np.array([
         [heights[0]],  # Defines matrix/vector for the histogram height. NOTE should match the order of poly
-         [heights[len(edges) // 10]],
-         [x[len(edges) // 2]],
-         [x[len(edges) // -5]],
-         [x[len(edges) // -9]],
-         [heights[int(len(edges) // 1.1)]],
-         [heights[-1]]])
+        [heights[int(len(edges) // 10)]],
+        [heights[int(len(edges) // 2)]],
+        [heights[int(len(edges) // -5)]],
+        [heights[int(len(edges) // -7)]],
+        [heights[int(len(edges) // -1.1)]],
+        [heights[int(len(edges) // 4)]],  # Comment out for appropriate order
+        [heights[int(len(edges) // 6)]],
+        [heights[int(len(edges) // 9)]],
+        [heights[int(len(edges) // -3)]],
+        [heights[int(len(edges) // -9)]],
+        [heights[int(len(edges) // -3.5)]],
+        [heights[-1]]])
 
     coeffs = np.linalg.solve(x_array, y_array)[:, 0]  # Solves for the coeffs of the polynomial
     coeffs, cov = curve_fit(poly, x, heights, p0=coeffs)
@@ -212,7 +226,6 @@ for bn in range(10):
     # plt.legend()
     # plt.show()
 
-
 #%%
 # Performs iMinuit fit for Afb, FL
 
@@ -225,10 +238,10 @@ bin_results_to_check = None
 
 log_likelihood.errordef = Minuit.LIKELIHOOD
 decimal_places = 3
-starting_point = [0.3, 0.5]  # 0.3, 0.5 best for signal
+starting_point = [0.8, 0.1]
 chi_fl, chi_afb = [], []
 
-# xx, yy = np.meshgrid(np.linspace(-1.0, 1.0, 10), np.linspace(-1.0, 1.0, 10))
+xx, yy = np.meshgrid(np.linspace(-1.0, 1.0, 10), np.linspace(0.0, 1.0, 10))
 
 # for a in xx[0, :]:
 #     for f in yy[:, 0]:
@@ -238,7 +251,7 @@ dummy_fl, dummy_afb = [], []
 for i in range(10):
     m = Minuit(log_likelihood, fl=starting_point[0], afb=starting_point[1], _bin=i)
     m.fixed['_bin'] = True
-    m.limits = ((-1.0, 1.0), (-1.0, 1.0), None)
+    m.limits = ((0.0, 1.0), (-1.0, 1.0), None)
     m.migrad()
     m.hesse()
     if i == bin_number_to_check:
@@ -251,8 +264,8 @@ for i in range(10):
     afb_errs.append(m.errors[1])
     print(f"Bin {i}: {np.round(fls[i], decimal_places)} +/- {np.round(fl_errs[i], decimal_places)},",
           f"{np.round(afbs[i], decimal_places)} +/- {np.round(afb_errs[i], decimal_places)}. valid?: {m.fmin.is_valid}")
-        # chi_fl.append(np.sum(dummy_fl))
-        # chi_afb.append(np.sum(dummy_afb))
+# chi_fl.append(np.sum(dummy_fl))
+# chi_afb.append(np.sum(dummy_afb))
 #%% check afb and fl
 # =============================================================================
 # plt.figure(figsize=(8, 5))
@@ -267,20 +280,20 @@ for i in range(10):
 
 #%% check pdf
 # =============================================================================
-bin_to_plot = bin_number_to_check
-number_of_bins_in_hist = 70
-cos_theta_l_bin = bins[bin_to_plot]
-plt.figure()
-hist, _bins, _ = plt.hist(cos_theta_l_bin, bins=number_of_bins_in_hist)
-x = np.linspace(-1, 1, number_of_bins_in_hist)
-pdf_multiplier = np.sum(hist) * (np.max(cos_theta_l_bin) - np.min(cos_theta_l_bin)) / number_of_bins_in_hist
-y = d2gamma_p_d2q2_dcostheta(fl=fls[bin_to_plot], afb=afbs[bin_to_plot], cos_theta_l=x, bn=bin_to_plot) * pdf_multiplier
-plt.plot(x, y, label=f'Fit for bin {bin_to_plot}')
-plt.xlabel(r'$cos(\theta_l)$')
-plt.ylabel(r'Number of candidates')
-plt.legend()
-plt.grid()
-plt.show()
+# bin_to_plot = bin_number_to_check
+# number_of_bins_in_hist = 70
+# cos_theta_l_bin = bins[bin_to_plot]
+# plt.figure()
+# hist, _bins, _ = plt.hist(cos_theta_l_bin, bins=number_of_bins_in_hist)
+# x = np.linspace(-1, 1, number_of_bins_in_hist)
+# pdf_multiplier = np.sum(hist) * (np.max(cos_theta_l_bin) - np.min(cos_theta_l_bin)) / number_of_bins_in_hist
+# y = d2gamma_p_d2q2_dcostheta(fl=fls[bin_to_plot], afb=afbs[bin_to_plot], cos_theta_l=x, bn=bin_to_plot) * pdf_multiplier
+# plt.plot(x, y, label=f'Fit for bin {bin_to_plot}')
+# plt.xlabel(r'$cos(\theta_l)$')
+# plt.ylabel(r'Number of candidates')
+# plt.legend()
+# plt.grid()
+# plt.show()
 # =============================================================================
 
 # Plots fitted values
@@ -331,7 +344,6 @@ ax.set_ylabel('fl')
 ax.set_zlabel('sum of mean squared')
 ax.plot(xx.reshape(-1), yy.reshape(-1), 'kx', zdir='z', markersize=1)
 
-
 #%%
 '''
 NOT COMPLETE    
@@ -345,7 +357,6 @@ z = []
 for x in xx:
     for y in yy:
         z.append(log_likelihood(x, y, 0))
-
 
 ax = plt.axes(projection='3d')
 ax.plot_wireframe(xx, yy, z)
